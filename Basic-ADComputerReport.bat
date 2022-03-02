85a556ff8c689dbe15f914316792894a252ade42
Kristopher Roy <kristopher.roy@gmail.com>
2022-03-02 14:33:34 -0800

    added script, bat, and howto

---
+++ Basic-ADComputerReport.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe /executionpolicy bypass /file %~dp0\Basic-ADComputerReport.ps1
---
+++ Basic-ADComputerReport.ps1
@@ -0,0 +1,10 @@
+﻿<#
+Created By: Kristopher Roy
+Last Updated By: BTL
+Created On: 02Mar2022
+Last Updated On: 02Mar2022
+#>
+
+Import-Module activedirectory
+
+Get-ADComputer -filter * -properties *|select *|export-csv c:\temp\computerreport.csv -NoTypeInformation
